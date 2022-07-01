12d5ef4752725b4400d2f702aae11e0bd9b6326d
#darafd <101981273+Darafd@users.noreply.github.com>
2022-07-01 08:29:59 +0600

    Update Practice2.java

--- Practice2.java
+++ Practice2.java
@@ -1,7 +1,28 @@
 package kz.practice.secondLesson;
+import java.util.Scanner;
 
 public class Practice2 {
+/* Задание 7
+    Напишите программу, где ввожу два целых числа. Если они отличаются на 100, то выведите YES, иначе NO.
+    Ввод:   100
+            200
+    Вывод:  YES
+
+    Ввод:   100
+            189
+    Вывод:  NO
+*/
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+        int b = in.nextInt();
+            
+        if(a - b && b - a < 100) {
+            System.out.println("Yes");
+        } else { System.out.println("No");
+               }
+        
 
     }
 }
